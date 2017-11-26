3cdacfc6ae8c025f66b0696208a7d5b8beef3f68
Yaznas <sanjshrestha53@gmail.com>
2017-11-26 18:23:02 +0545

    bullhead: Candy-fy

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -29,12 +29,12 @@ PRODUCT_DEVICE := bullhead
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on BullHead
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+#PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
 
-PRODUCT_PACKAGES += \
-    Launcher3 \
-    WallpaperPicker
+#PRODUCT_PACKAGES += \
+    #Launcher3 \
+    #WallpaperPicker
 
