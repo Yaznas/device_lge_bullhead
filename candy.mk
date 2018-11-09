123136456dcd3ed8cded9bce69e69aa1e06a9645
Yaznas <sanjshrestha53@gmail.com>
2018-11-10 00:06:58 +0545

    bullhead: Update fingerprint 8.1.0(OPM7.181105.004, r50, NOV 2018)

--- candy.mk
+++ candy.mk
@@ -20,5 +20,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM7.181005.003/4984324:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM7.181005.003 4984324 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM7.181105.004/5038062:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM7.181105.004 5038062 release-keys"
